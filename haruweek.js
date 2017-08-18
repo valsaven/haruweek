a074fa70716ae7ba00c53bd7ea71dcf39302f2ef
Val Saven <val.saven@gmail.com>
2017-08-18 12:18:01 +0300

    Add const for port

--- haruweek.js
+++ haruweek.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('Listening on port 3000...'));
 
 const today = new Date().getDay();
 
