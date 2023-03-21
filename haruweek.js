90607ccce3604582d640fa6bffacd831765a2d49
Val Saven <val.saven@gmail.com>
2023-03-21 17:50:47 +0300

    Refactor: use ECMAScript modules, async/await, error handling

--- haruweek.js
+++ haruweek.js
@@ -1,24 +1,34 @@
-const http = require('http');
-const fileSystem = require('fs');
-const path = require('path');
+import http from 'http';
+import path from 'path';
+import { promises as fsPromises, createReadStream } from 'fs';
 
 const port = process.env.PORT || 3000;
 
 console.log(`Listening on port ${port}...`);
 
-const requestListener = function (req, res) {
-  const today = new Date().getDay();
-
-  const filePath = path.join(__dirname, `public/${today}.jpg`);
-  const stat = fileSystem.statSync(filePath);
-
+const setHeaders = (res, contentType, contentLength) => {
   res.writeHead(200, {
-    'Content-Type': 'image/jpeg',
-    'Content-Length': stat.size
+    'Content-Type': contentType,
+    'Content-Length': contentLength
   });
+};
 
-  const readStream = fileSystem.createReadStream(filePath);
-  readStream.pipe(res);
-}
+const requestListener = async (req, res) => {
+  const today = new Date().getDay();
+  const filePath = path.join(process.cwd(), `public/${today}.jpg`);
+
+  try {
+    const stat = await fsPromises.stat(filePath);
+    setHeaders(res, 'image/jpeg', stat.size);
+
+    const readStream = createReadStream(filePath);
+    readStream.pipe(res);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal server error');
+  }
+};
 
 http.createServer(requestListener).listen(port);
